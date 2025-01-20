ffd800122ee76919cd94d8af333376935808c82b
ar1234567898 <24a_tav@liceum.ztu.edu.ua>
2025-01-20 08:32:25 +0200

    Update my-library.js

--- common/my-library.js
+++ common/my-library.js
@@ -1,6 +1,6 @@
 function getNumericValue(message) {
     while(true) {
-        let value = prompt(message);;
+        let value = prompt(message);
         let isValid = !isNaN(value) && value !== "" && value !== null;
         if(isValid) return Number(value);
     }
